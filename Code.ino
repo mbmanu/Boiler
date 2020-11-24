0a21810977016a662a5a41f1ea580dc5641455b5
Manu <mbmanu74@gmail.com>
2020-11-24 18:31:46 +0530

    Modified code

--- Code.ino
+++ Code.ino
@@ -1,35 +1,35 @@
 #include<Servo.h>
-const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
-Servo myservo;
+const int trigPin = 7;      // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6;      // Echo Pin of Ultrasonic Sensor
+Servo myservo;              // Creating an object for Servo class
 
 void setup() {
-   Serial.begin(9600); // Starting Serial Terminal
-   myservo.attach(9);
+   Serial.begin(9600);      // Serial Communication is starting with 9600 of baudrate speed
+   myservo.attach(9);       // attach servo to pin 9 
+   pinMode(trigPin, OUTPUT);// Sets the trigPin as an OUTPUT
+   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 }
 
-void loop() {
+void loop() {               // infinte loop
+  
    long duration, cm;
-   pinMode(pingPin, OUTPUT);
-   digitalWrite(pingPin, LOW);
+  
+   digitalWrite(trigPin, LOW);     // Clears the trigPin condition
    delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
-   cm = microsecondsToCentimeters(duration);
-   Serial.print(cm);
+   digitalWrite(trigPin, HIGH);    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+   delayMicroseconds(10);         
+   digitalWrite(trigPin, LOW);
+   
+   duration = pulseIn(echoPin, HIGH);  // Reads the echoPin, returns the sound wave travel time in microseconds
+   
+   cm = duration * 0.034 / 2;   // convert duration to cm
+   Serial.print(cm);    //Printing distance in the serial monitor
    Serial.print("cm");
    Serial.println();
-   if(cm < 10){
-     myservo.write(180);
-     delay(500);
-     myservo.write(0);
-     delay(500);}
-   delay(100);
-}
-
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
+   if(cm < 10){         // if the distance is below 10cm
+     myservo.write(45); // Rotate the servo by 45 degrees
+     delay(500);        // delay of 500 milliseconds(for the servo)
+     myservo.write(0);  // Rotate the servo back to the original position
+     delay(500);}       
+   delay(100);          // delay for the sensor (100 milliseconds)
 }
